52530656c6a34f52fb845e299af9d3967af6882d
Harry Borrelli <hborrelli@outlook.com>
2020-07-27 23:27:06 -0600

    Adjust how html was being rendered.

--- src/components/RecipeItem.vue
+++ src/components/RecipeItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="recipe-item">
     <h4>{{ recipe.title }}</h4>
     <p>{{ recipe.instructions }}</p>
   </div>
@@ -13,5 +13,7 @@
 </script>
 
 <style scoped>
-
+  .recipe-item {
+    width:33.3%;
+  }
 </style>
--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -3,12 +3,10 @@
     <header>
       <h3>Recipes:</h3>
     </header>
-    <div 
-      class='recipes-list'
-      v-for='recipe in recipes'
-      v-bind:key='recipe.id'
-    >
+    <div class='recipes-list'>
       <RecipeItem 
+        v-for='recipe in recipes'
+        v-bind:key='recipe.id'
         v-bind:recipe='recipe'
       />
     </div>
@@ -28,5 +26,8 @@
 </script>
 
 <style scoped>
-
+  .recipes-list {
+    display: flex;
+    flex-flow:wrap;
+  }
 </style>
