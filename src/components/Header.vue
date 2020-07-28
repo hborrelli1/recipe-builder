c96d562880654988645ce0f9f35c0498fbaf3965
Harry Borrelli <hborrelli@outlook.com>
2020-07-27 22:01:18 -0600

    Add Header component.

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Recipe Builder</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Header'
+  }
+</script>
+
+<style scoped>
+
+</style>
