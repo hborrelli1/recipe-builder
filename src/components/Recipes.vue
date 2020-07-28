55c962932d5a2df07f8731a8af6b5dd2a00ceffc
Harry Borrelli <hborrelli@outlook.com>
2020-07-27 22:33:13 -0600

    Move data from App into Home. Add Recipes component.

---
+++ src/components/Recipes.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <header>
+      <h3>Recipes:</h3>
+    </header>
+    <div 
+      class='recipes-list'
+      v-for='recipe in recipes'
+      v-bind:key='recipe.id'
+    >
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'Recipes',
+    props: ['recipes']
+  }
+</script>
+
+<style scoped>
+
+</style>
